7cd1ddc8e7f12196730329f26b68575255a7f290
nch <nch@netcompany.com>
2019-03-24 16:00:08 +0100

    stub exerciseView class

--- src/main/java/com/workout/workoutArtifact/endpoint/ui/views/ExerciseView.java
+++ src/main/java/com/workout/workoutArtifact/endpoint/ui/views/ExerciseView.java
@@ -1,5 +1,23 @@
 package com.workout.workoutArtifact.endpoint.ui.views;
 
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.workout.workoutArtifact.endpoint.facade.ExerciseFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route(value = "exercises")
+@PageTitle("Exercises overview")
 public class ExerciseView {
+
+  ExerciseFacade exerciseFacade;
+
+  @Autowired
+  public ExerciseView(ExerciseFacade exerciseFacade) {
+    this.exerciseFacade = exerciseFacade;
+
+
+  }
+
+
 
 }
