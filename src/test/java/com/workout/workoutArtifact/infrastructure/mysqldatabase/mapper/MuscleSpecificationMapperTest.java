a635b47745c9a68cc893ea8586d171bb120d69d2
nch <nch@netcompany.com>
2019-08-10 10:51:57 +0200

    Add tests to MuscleSpecificationTest

--- src/test/java/com/workout/workoutArtifact/infrastructure/mysqldatabase/mapper/MuscleSpecificationMapperTest.java
+++ src/test/java/com/workout/workoutArtifact/infrastructure/mysqldatabase/mapper/MuscleSpecificationMapperTest.java
@@ -1,15 +1,28 @@
 package com.workout.workoutArtifact.infrastructure.mysqldatabase.mapper;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import com.workout.workoutArtifact.domain.muscle.model.Muscle;
+import com.workout.workoutArtifact.domain.muscle.model.Muscle.NameSpecification;
+import com.workout.workoutArtifact.infrastructure.mysqldatabase.entity.MuscleEntity;
+import java.util.Arrays;
 import org.junit.Test;
 
 public class MuscleSpecificationMapperTest {
 
-  @Test
-  public void name() {
+  MuscleSpecificationMapper muscleSpecificationMapper = new MuscleSpecificationMapper();
 
-    assertThat(true, is(false));
+  @Test
+  public void mapsNameSpecification() {
+    org.springframework.data.jpa.domain.Specification<MuscleEntity> jpaSpecification = muscleSpecificationMapper.toJpaSpecification(new NameSpecification(Arrays.asList("some_name")));
+    assertThat(jpaSpecification, is(instanceOf(org.springframework.data.jpa.domain.Specification.class)));
+  }
+
+  @Test
+  public void mapsBodyPartSpecification() {
+    org.springframework.data.jpa.domain.Specification<MuscleEntity> jpaSpecification = muscleSpecificationMapper.toJpaSpecification(new Muscle.BodyPartSpecification(Arrays.asList("some_body_part")));
+    assertThat(jpaSpecification, is(instanceOf(org.springframework.data.jpa.domain.Specification.class)));
   }
 }
