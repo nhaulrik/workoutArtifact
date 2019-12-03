e71b1277a00efdd66295826878adaac7c7041150
nch <nch@netcompany.com>
2019-12-03 20:34:35 +0100

    disregard integration test

--- src/test/java/com/workout/workoutArtifact/endpoint/controller/GraphQLControllerTest.java
+++ src/test/java/com/workout/workoutArtifact/endpoint/controller/GraphQLControllerTest.java
@@ -24,7 +24,6 @@
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.http.MediaType;
 //import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.junit4.SpringRunner;
 //import org.springframework.test.util.ReflectionTestUtils;
 //import org.springframework.test.web.servlet.MockMvc;
 //
