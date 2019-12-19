97fc9e119a90d819fc1e89e9b040b8820985fef1
nch <nch@netcompany.com>
2019-12-19 19:58:19 +0100

    add working docker-compose

--- src/main/resources/db/migration/V1__Base_tables.sql
+++ src/main/resources/db/migration/V1__Base_tables.sql
@@ -2,17 +2,15 @@
 -- Table structure for table `user`
 --
 DROP TABLE IF EXISTS `user`;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `user`
-(
+ SET character_set_client = utf8 ;
+CREATE TABLE `user` (
 	`id` bigint(20) NOT NULL auto_increment,
 	`birth_day` date NULL,
 	`first_name` varchar(255) NULL,
 	`gender` varchar(255) NULL,
 	`last_name` varchar(255) NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 --
@@ -20,34 +18,34 @@ CREATE TABLE `user`
 --
 
 DROP TABLE IF EXISTS `exercise`;
- SET character_set_client = utf8mb4 ;
+ SET character_set_client = utf8 ;
 CREATE TABLE `exercise` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `body_part` varchar(255) DEFAULT NULL,
   `is_compound` bit(1) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `muscle`
 --
 
 DROP TABLE IF EXISTS `muscle`;
- SET character_set_client = utf8mb4 ;
+ SET character_set_client = utf8 ;
 CREATE TABLE `muscle` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `body_part` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `exercise_muscle_relation`
 --
 
 DROP TABLE IF EXISTS `exercise_muscle_relation`;
- SET character_set_client = utf8mb4 ;
+ SET character_set_client = utf8 ;
 CREATE TABLE `exercise_muscle_relation` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `ratio` int(11) DEFAULT NULL,
@@ -58,14 +56,14 @@ CREATE TABLE `exercise_muscle_relation` (
   KEY `FKehi12yt81x1omut7w2mwkw8aa` (`muscle_entity_id`),
   CONSTRAINT `FKehi12yt81x1omut7w2mwkw8aa` FOREIGN KEY (`muscle_entity_id`) REFERENCES `muscle` (`id`),
   CONSTRAINT `FKkxfradqvr65o99d2jcm9kf7qu` FOREIGN KEY (`exercise_entity_id`) REFERENCES `exercise` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `session`
 --
 
 DROP TABLE IF EXISTS `session`;
- SET character_set_client = utf8mb4 ;
+ SET character_set_client = utf8 ;
 CREATE TABLE `session` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `creation_date_time` datetime(6) DEFAULT NULL,
@@ -73,14 +71,14 @@ CREATE TABLE `session` (
   `programme` varchar(255) DEFAULT NULL,
   `split_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `workoutset`
 --
 
 DROP TABLE IF EXISTS `workoutset`;
- SET character_set_client = utf8mb4 ;
+ SET character_set_client = utf8 ;
 CREATE TABLE `workoutset` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `repetition_maximum` int(11) DEFAULT NULL,
@@ -95,4 +93,4 @@ CREATE TABLE `workoutset` (
   KEY `FK7j0d2f3j3y94bnpv3po9rtmff` (`session_entity_id`),
   CONSTRAINT `FK7j0d2f3j3y94bnpv3po9rtmff` FOREIGN KEY (`session_entity_id`) REFERENCES `session` (`id`),
   CONSTRAINT `FKhte4id6wwyx3y6nhojt9aq9rt` FOREIGN KEY (`exercise_entity_id`) REFERENCES `exercise` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
