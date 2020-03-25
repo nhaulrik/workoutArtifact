376eeff5da31364ec9e134b714c165733809f121
Nikolaj Haulrik <nikolajmx@hotmail.com>
2020-03-25 19:44:12 +0100

    add cross origins

---
+++ src/main/java/com/workout/workoutArtifact/WebConfig.java
@@ -0,0 +1,4 @@
+package com.workout.workoutArtifact;
+
+public class WebConfig {
+}
--- src/main/java/com/workout/workoutArtifact/endpoint/controller/GraphQLController.java
+++ src/main/java/com/workout/workoutArtifact/endpoint/controller/GraphQLController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ public class GraphQLController {
 
   private final GraphQL graphQL;
 
+  @CrossOrigin(origins = {"http://localhost:4200", "http://46.101.126.222:4200"})
   @RequestMapping(value = "/graphql", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
   @ResponseBody
   public Map<String, Object> graphql(
